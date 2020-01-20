4e6a517bfcd9f657b101cfd4ebc6e2a267de2476
Kazeem Oluwatobi Omoloja <omolojakazeem@gmail.com>
2020-01-20 22:18:36 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,26 +1,45 @@
-customer_details = [{
+#default Data for Testing
+customer_details = [
+    {
     "Email": "omoloja.kazeem@gmail.com",
     "Password": "123456",
     "Balance":120
-},{
+},
+    {
     "Email": "omoloja.kaz@gmail.com",
     "Password": "Olute",
     "Balance":100.0
-}]
+}
+]
 
 
 def initialize():
     option = input("""
-    Press 1: Create Account
-    Press 2: Transaction >
+Press 1: Create Account
+Press 2: Transaction 
+Pres Quit: Exit >
     """)
-    if option == '1':
-        create_account()
-    elif option == '2':
-        perform_transaction()
-    else:
-        print("I don't Understand")
-        create_account()
+    while True:
+        if option == '1':
+            create_account()
+        elif option == '2':
+            perform_transaction()
+        elif option == 'quit':
+            break
+        else:
+            option = input("""
+            I don't Understand!
+            Would you like to create an account?
+            Press 1: Create Account
+            Press 2: Transaction >
+            """)
+            if option == '1':
+                create_account()
+            elif option == '2':
+                perform_transaction()
+            else:
+                print('Stop Stalking, I am so secured')
+
 
 def create_account():
     print("""To create an Account, you would need an 
@@ -40,9 +59,9 @@ def create_account():
             
     else:
         print("User already Exist")
+
     
 def perform_transaction():
-    
     user_email = input("Enter your Email: ")
     user_pass = input("Enter your Password: ")
     for data in customer_details:
@@ -56,17 +75,37 @@ def perform_transaction():
             """)
                 if option == '1':
                     print(f'Your Account Balance is: {data["Balance"]}')
+                    initialize()
                 elif option == '2':
                     deposit = int(input("How much do you want to deposit?: "))
                     data['Balance'] += deposit
+                    print(f"Your Balance is now: {data['Balance']}")
+                    initialize()
                 elif option == '3':
                     withdrawal = int(input("How much do you want to Withdraw?: "))
                     data['Balance'] -= withdrawal
+                    print(f"Your Balance is now: {data['Balance']}")
+                    initialize()
                 elif option == '4':
                     transfer = int(input("How much do you want to Transfer?: "))
-                    data['Balance'] -= transfer
+                    if data['Balance'] > transfer:
+                        recepient_email = input("Enter receipient Email: ")
+                        data['Balance'] -= transfer
+                        for n in customer_details:
+                            if n['Email'] == recepient_email:
+                                n['Balance'] += transfer
+                                print(f"Your Balance is now: {data['Balance']}")
+                                print(f"{transfer} has been transfered to {n['Email']}")
+                                initialize()
+                            else:
+                                print("User Does not Exist")
+                                initialize()
+                    else:
+                        print("Insuficient Funds")
+                        initialize()
             else:
                 print("Can't Find User")
+                initialize()
         
 initialize()
 global option
